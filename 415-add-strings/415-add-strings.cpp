78d369c4743156471988bd27fdd07f2303502b8a
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2022-07-01 21:56:34 +0530

    Time: 38 ms (20.52%), Space: 55.7 MB (22.16%) - LeetHub

--- 415-add-strings/415-add-strings.cpp
+++ 415-add-strings/415-add-strings.cpp
@@ -3,45 +3,18 @@ public:
     string addStrings(string num1, string num2) {
         string ans = "";
         
-        int i = num1.size()-1, j = num2.size()-1, carry = 0;
-        while(i>=0 && j>=0){
-            int sum  = carry + num1[i] -'0' + num2[j]-'0' ;
+        int i = num1.size()-1, j = num2.size()-1, carry = 0, sum = 0;
+        while(i>=0 || j>=0){
+            sum = 0;
+            if(i>=0)
+                sum += num1[i--] -'0';
+            if(j>=0)
+                sum += num2[j--] -'0';
             
-            if(sum > 9){
-                sum = sum%10;
-                carry = 1;
-            }
-            else
-            carry = 0;
+            sum += carry;
+            carry = sum / 10;
+            sum %= 10;
             ans = to_string(sum) + ans ;
-            --i , --j;
-        }
-        while(i >= 0){
-            
-            int sum = carry + num1[i]-'0';
-            
-            if(sum > 9){
-                sum = sum%10;
-                carry = 1;
-            }
-            else
-            carry = 0;
-            ans = to_string(sum) + ans ;
-            --i;
-        }
-        
-        while(j >= 0){
-            
-            int sum = carry + num2[j]-'0';
-            
-            if(sum > 9){
-                sum = sum%10;
-                carry = 1;
-            }
-            else
-            carry = 0;
-            ans = to_string(sum) + ans ;
-            --j;
         }
         if(carry)
             return ("1"+ans);
