9c7b3c42e94125ed7fb29652f75eef8ccc308bd0
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2022-07-03 15:53:42 +0530

    Time: 62 ms (12.73%), Space: 9.8 MB (94.54%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-        /*
-        1. if open paranethesis then will insert index in set or map
-        2. if close ) encountered
-            a) if opening is encountered before then will simply remove the index of openinng paraenthesis from set or map
-            b) if open not entcounteres then will delete this charater
-        */
-        
         vector<int> temp;
         for(int i=0; i<s.size(); i++){
             if(s[i]=='(')
                 temp.push_back(i);
             else if(s[i]==')'){
                 if(temp.size()==0)
-                    s.erase(s.begin()+i--);
+                    s.erase(s.begin()+i),i--;
                 else
                     temp.pop_back();
             }
