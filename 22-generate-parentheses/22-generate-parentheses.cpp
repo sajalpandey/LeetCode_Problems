cea500a9bbb12e132b77b61ee906659a388c9481
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2022-07-04 20:56:18 +0530

    Time: 3 ms (86.13%), Space: 13.7 MB (47.97%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    void static recursiveFun(vector<string> &result, string str, int l ,int r){
-        if(l==0 && r==0){
+    void static recursiveFun(vector<string> &result, string str, int l ,int r, int length){
+        if(str.size()==2*length){
             result.emplace_back(str);
             return ;
         }
-        if(r<l || l < 0)
-            return;
-        recursiveFun(result, (str+"("), l-1, r);
-        recursiveFun(result, (str+")"), l, r-1);
+        if(l<length)
+            recursiveFun(result, (str+"("), l+1, r,length);
+        if(r<l)
+            recursiveFun(result, (str+")"), l, r+1,length);
     }
     vector<string> generateParenthesis(int n) {
         string str = "";
         vector<string> result;
         
-        recursiveFun(result, str , n, n);
+        recursiveFun(result, str , 0, 0, n);
         return result;
     }
 };
