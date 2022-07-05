bea5ca9c4bbbcb8c763d3f0d30d7e4cbb64ea8bb
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2022-07-05 22:50:25 +0530

    Time: 6 ms (70.97%), Space: 7 MB (90.04%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
     string reverseWords(string s) {
+        /*
+        1. Remove the leading spaces
+        2. Copy the word at the positions of spaces
+        3. Now reverse the word
+        4. resize the array
+        5. reverse the whole string and return the result
+        */
+        int i = 0, j = 0, noOfWords = 0, left = 0;
         
-        string result = "" ,temp = "";
-        int i=0;
-        while(i<s.size() && s[i]==' ')
-            i++;
-        for(; i<s.size();){
+        while(i<s.size()){
             
-            if(isalnum(s[i])){
-                temp = temp + (s[i++]);
-            }
-            else if(s[i]== ' '){
-                reverse(temp.begin(),temp.end());
-                result += temp;
-                result += " ";
-                temp = "";
-                while(i<s.size() && s[i]==' ')
-                    i++;
-            }  
+            while(i<size(s) && s[i] == ' ')
+                i++;
+            if(i<size(s) && noOfWords)
+                s[j++] = ' ';
+            left = j;
+            while(i<size(s) && s[i]!=' ')
+                s[j++] = s[i++];
+            reverse(s.begin()+left, s.begin()+j);
+            
+            noOfWords++;
         }
-        reverse(temp.begin(),temp.end());
-        result += temp;
-        reverse(result.begin(),result.end());
-        i = 0;
-        while(i<result.size() && result[i] == ' ')
-            i++;
-        result.erase(result.begin(),result.begin()+i);
-        return result;
-        //return "";
+        s.resize(j);
+        reverse(s.begin(),s.end());
+        return s;
     }
 };
