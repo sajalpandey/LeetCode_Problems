370a3e86ba4b0fbf04137d2bf52c918e1186a2e9
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2022-07-09 17:56:20 +0530

    Time: 37 ms (8.36%), Space: 13.1 MB (81.72%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,22 +1,49 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        unordered_set<int> s ,r;
+        int firstRow = 0 , firstCol = 0;
+        int row = matrix.size(), col = matrix[0].size();
+        //check for 1st Row
+        for(int i = 0; i< col; i++)
+            if(matrix[0][i]==0){
+                firstRow = 1;break;
+            }
+        //check for 1st Col
+        for(int j = 0; j<row; j++)
+            if(matrix[j][0] == 0){
+                firstCol = 1;break;
+            }
         
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[0].size();j++)
-            {
-                if(matrix[i][j]==0)
-                    s.insert(i),r.insert(j);
+        //Store IF any ele is zero Strore it in first row and column
+        
+        for(int i=1; i<row; i++)
+        {
+            for(int j=1; j<col; j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
             }
         }
         
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[0].size();j++)
-            {
-                if(s.count(i) || r.count(j))
-                    matrix[i][j] = 0;
+        for(int i=1; i<row; i++){
+            for(int j=1; j<col; j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0)
+                    matrix[i][j]=0;
             }
         }
+    
+        if(firstRow)
+        {
+            for(int i =0 ;i<col; i++)
+                matrix[0][i] = 0;
+        }
+
+        if(firstCol)
+        {
+            for(int j=0;j<row;j++)
+                matrix[j][0] =0;
+        }
+        
     }
 };
