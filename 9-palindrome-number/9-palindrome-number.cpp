01f2c6bed27e8e9737ce147e76a6936d20e23d0f
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2022-07-13 20:18:55 +0530

    Time: 26 ms (38.92%), Space: 5.9 MB (31.51%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int reverse(int x){
+        int res = 0, sign = 1, last_digit;
+        if( x < 0)
+            sign = -1;
+        x = abs(x);
+        while( x > 0 )
+        {
+            last_digit = x % 10;
+            if( res  > INT_MAX / 10)
+                return 0;
+            res = res * 10 + last_digit;
+            x /= 10;
+        }
+        
+        if(sign < 0 && sign * res < INT_MIN)
+            return 0;
+        return (sign * res);
+    }
+    
+    bool isPalindrome(int x) {
+        if( x < 0)
+            return false;
+        int reverseNum = reverse(x);
+        if( x == reverseNum )
+            return true;
+        else
+            return false;
+    }
+};
