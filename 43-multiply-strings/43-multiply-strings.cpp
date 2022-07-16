69a712a06d53e449aae473bff04426e650310577
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2022-07-16 23:11:03 +0530

    Time: 7 ms (81.54%), Space: 7.8 MB (33.60%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.cpp
+++ 43-multiply-strings/43-multiply-strings.cpp
@@ -1,46 +1,24 @@
 class Solution {
 public:
-    string addStrings(string first , string second){
-        int i = size(first) - 1, j = size(second) - 1, carry = 0, sum = 0;
-        string res = "";
-        while(i >= 0 || j >= 0){
-            if(i >= 0)
-                sum += first[i--] - '0';
-            if(j >= 0)
-                sum += second[j--] - '0';
-            sum += carry;
-            if(sum > 9){
-                sum %= 10;
-                carry = 1;
-            }
-            else
-                carry = 0;
-            
-            res = to_string(sum) + res;
-            sum = 0;
-        }
-        if(carry)
-            return ("1"+res);
-        return res ;
-    }
     string multiply(string num1, string num2) {
-        string res = "" , tempSum = "";
+        string res = "";
         if(num1 == "0" || num2 == "0")
             return "0";
-        int i =  0;
-        while(i < size(num2)){
-            int digit  = num2[i] - '0';
-            tempSum = "";
-            while(digit > 0){
-                tempSum  = addStrings(tempSum, num1);              
-                digit--;
-            }
-            res = addStrings(res, tempSum);
-            if(i != size(num2) - 1)
-                res += "0";
-            i++;
-        }
         
+        vector<int> temp(size(num1) + size(num2), 0);
+        
+        for(int i=num1.size()-1; i >= 0 ;--i){
+            for(int j = num2.size()-1; j >= 0; --j){
+                temp[i+j+1] += (num1[i] - '0') * (num2[j] - '0');
+                temp[i+j] += temp[i+j+1]/10;
+                temp[i+j+1] %= 10; 
+            }
+        }
+        int i = 0;
+        while(i<size(temp) && temp[i] == 0)
+            i++;
+        while(i < size(temp))
+            res = res + to_string(temp[i++]) ;
         return res;
     }
 };
