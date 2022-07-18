da41bfc1f8377d3000210e622aa4ac1c30c986d6
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2022-07-18 23:01:42 +0530

    Time: 0 ms (100.00%), Space: 6 MB (57.49%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
     int kthFactor(int n, int k) {
-        int count = 0;
         if(k > n)
             return -1;
-        for(int i = 1; i <= n/2; i++){
+        int i;
+        for(i = 1; i * i <= n; i++){
             if(n % i == 0)
-                count++;
-            if(count == k)
+                k--;
+            if(k == 0)
                 return i;
         }
-        if(++count == k)
-            return n;
+        i--;
+        if(i * i == n) i--;
+        for(; i > 0; i--){
+            if(n % i == 0)
+                k--;
+            if(k == 0)
+                return n/i;
+        }
         return -1;
     }
 };
