974cc0fa70cce2e21ceab272d0fade23f2eb058a
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2022-07-26 23:24:23 +0530

    Time: 330 ms (21.10%), Space: 63.2 MB (64.74%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,26 +1,9 @@
 class Solution {
 public:
-    int BouquetsPossible(vector<int>& bloomDay, int m, int k, int mid){
-        
-        int boq = 0, count = 0;
-        for(int i = 0; i < size(bloomDay); i++){
-            if(bloomDay[i] <= mid)
-                count++;
-            else
-                count = 0;
-            if(count == k){
-                boq++;
-                count = 0;
-            }
-            
-        }
-        return boq;
-    }
     int minDays(vector<int>& bloomDay, int m, int k) {
         int n = size(bloomDay);
         if(n < m*k)
             return -1;
-        
         int start = INT_MAX, end = INT_MIN, mid, ans;
         for(int i = 0; i < n; i++){
             if(start > bloomDay[i])
@@ -30,8 +13,18 @@ public:
         }
         while(start <= end){
             mid = start + (end - start)/2;
-            int temp = BouquetsPossible(bloomDay, m, k, mid);
-            if(temp >= m){
+            int boq = 0, count = 0;
+            for(int i = 0; i < size(bloomDay); i++){
+                if(bloomDay[i] <= mid)
+                    count++;
+                else
+                    count = 0;
+                if(count == k){
+                    boq++;
+                    count = 0;
+                }
+            }
+            if(boq >= m){
                 ans = mid;
                 end = mid - 1;
             }
