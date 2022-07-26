21e479408e44169d5848eab112b76876129acb38
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2022-07-26 20:55:02 +0530

    Time: 94 ms (42.41%), Space: 26 MB (67.26%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool shippingPossible(vector<int>& weights, int mid, int days){
-        
         int capacity = 0;
         for(int i = 0; i < size(weights); i++){
             if(capacity + weights[i] <= mid)
@@ -10,17 +9,16 @@ public:
                 days--;
                 if(days == 0 || weights[i] > mid)
                     return false;
-                
                 capacity = weights[i];
             }
         }
         return true;
     }
     int shipWithinDays(vector<int>& weights, int days) {
-        int start = INT_MAX, end = 0, mid, ans = -1;
+        int start = INT_MIN, end = 0, mid, ans = -1;
         for(auto &ele : weights){
             end += ele;
-            if(ele < start)
+            if(ele > start)
                 start = ele;
         }
         
