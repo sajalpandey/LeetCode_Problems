5086731e99bb647f2cfc1916f42c5ab27ba2acfd
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2022-08-15 16:56:12 +0530

    Time: 95 ms (21.42%), Space: 9.5 MB (52.35%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void generatePermutations(int pos, vector<int>& nums, set<vector<int>> &s){
+        
+        if(pos >= nums.size()){
+            s.insert(nums);
+            return;
+        }
+        for(int i = pos; i < nums.size(); i++){
+            swap(nums[i], nums[pos]);
+            generatePermutations(pos+1, nums, s);
+            swap(nums[i], nums[pos]);
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        set<vector<int>> s;
+        vector<vector<int>> ans;
+        generatePermutations(0, nums, s);
+        for(auto &vec: s)
+            ans.push_back(vec);
+        return ans;
+    }
+};
