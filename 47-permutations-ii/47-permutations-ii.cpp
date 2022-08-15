67ec8c69182b692e1264d5957821580d352599c7
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2022-08-15 17:14:04 +0530

    Time: 10 ms (73.92%), Space: 11.1 MB (24.37%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    void generatePermutations(int pos, vector<int>& nums, set<vector<int>> &s){
-        
+    void generatePermutations(int pos, vector<int>& nums,  vector<vector<int>> &ans){
         if(pos >= nums.size()){
-            s.insert(nums);
+            ans.push_back(nums);
             return;
         }
+        unordered_set<int> s;
         for(int i = pos; i < nums.size(); i++){
+            if(s.count(nums[i])) continue;
+            s.insert(nums[i]);
             swap(nums[i], nums[pos]);
-            generatePermutations(pos+1, nums, s);
+            generatePermutations(pos+1, nums, ans);
             swap(nums[i], nums[pos]);
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        set<vector<int>> s;
         vector<vector<int>> ans;
-        generatePermutations(0, nums, s);
-        for(auto &vec: s)
-            ans.emplace_back(vec);
+        generatePermutations(0, nums, ans);
         return ans;
     }
 };
