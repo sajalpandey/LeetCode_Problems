b29ba50653390965da85c7ef82bd809c81391e82
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2022-08-16 22:56:03 +0530

    Time: 48 ms (45.47%), Space: 9 MB (91.33%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    void generateCom(int i, int n, int k, vector<vector<int>> &ans, vector<int> &temp){
-        
+    void generateCom(int start, int n, int k, vector<vector<int>> &ans, vector<int> &temp){  
         if(temp.size() == k){
             ans.push_back(temp);
             return;
         }
-        if(i > n)
+        if(start >= n)
             return;
-        temp.emplace_back(i);
-        generateCom(i+1, n, k, ans, temp);
-        //skip this ele
-        temp.pop_back();
-        generateCom(i+1, n, k, ans, temp);
+        for(int i = start; i < n; i++){
+            temp.emplace_back(i+1);
+            generateCom(i+1, n, k, ans, temp);
+            temp.pop_back();
+        }
     }
     vector<vector<int>> combine(int n, int k) {
         vector<vector<int>> ans;
         vector<int> temp;
-        generateCom(1, n, k, ans, temp);
+        generateCom(0, n, k, ans, temp);
         return ans;
     }
 };
