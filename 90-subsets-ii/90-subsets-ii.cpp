d14c126c79eb7c5dd3964292ce141e1d038ac97e
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2022-08-16 21:52:35 +0530

    Time: 13 ms (21.76%), Space: 16.5 MB (19.58%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    void generateSubset(vector<int>& nums, int i, set<vector<int>> &s, vector<int> subset){
+    void generateSubset(vector<int>& nums, int i, vector<vector<int>> &ans, vector<int> subset){
         if(i >= nums.size()){
-            //sort(subset.begin(),subset.end());
-            s.insert(subset);
+            ans.push_back(subset);
             return;
         }
         subset.emplace_back(nums[i]);
-        generateSubset(nums, i+1, s, subset);
+        generateSubset(nums, i+1, ans, subset);
         subset.pop_back();
-        generateSubset(nums, i+1, s, subset);
+        while(i+1 < nums.size() && nums[i] == nums[i+1]) i++;
+        generateSubset(nums, i+1, ans, subset);
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> subset;
         set<vector<int>> s;
         sort(nums.begin(),nums.end());
-        generateSubset(nums, 0, s, subset);
-        for(auto &ele: s)
-            ans.push_back(ele);
+        generateSubset(nums, 0, ans, subset);
         return ans;
     }
 };
