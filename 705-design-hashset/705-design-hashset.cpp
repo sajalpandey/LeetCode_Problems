24149f44f4ec2a56c37b909e1fb55dcd964c674f
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2022-08-28 18:35:20 +0530

    Time: 114 ms (86.86%), Space: 41.2 MB (68.72%) - LeetHub

--- 705-design-hashset/705-design-hashset.cpp
+++ 705-design-hashset/705-design-hashset.cpp
@@ -1,22 +1,35 @@
 class MyHashSet {
 public:
-    vector<int> v;
+    vector<list<int>> hashSet;
     int size;
     MyHashSet() {
-        size = 1e6+1;
-        v.resize(size);
+        size = 100;
+        hashSet.resize(size);
     }
-    
+    int hashFun(int k){
+        return (k%size);
+    }
+    list<int> :: iterator search(int key){
+        int bucket = hashFun(key);
+        return find(hashSet[bucket].begin(),hashSet[bucket].end(),key);
+    }
     void add(int key) {
-        v[key] = 1;
+        if(contains(key) == true) return;
+        int bucket = hashFun(key);
+        hashSet[bucket].push_back(key);
     }
     
     void remove(int key) {
-        v[key] = 0;
+        if(contains(key) == false) return;
+        int bucket = hashFun(key);
+        hashSet[bucket].erase(search(key));
     }
     
     bool contains(int key) {
-        return v[key];
+        int bucket = hashFun(key);
+        if(search(key) != hashSet[bucket].end())
+            return true;
+        return false;
     }
 };
 
