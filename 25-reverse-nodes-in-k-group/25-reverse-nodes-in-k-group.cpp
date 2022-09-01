14423f933e276f6b4d3cc7a78e3abc415d86bcf4
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2022-09-01 20:08:08 +0530

    Time: 37 ms (11.87%), Space: 11.6 MB (29.78%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
@@ -22,16 +22,37 @@ public:
     }
     ListNode* reverseKGroup(ListNode* head, int k) {
         if(head == NULL || head->next == NULL || k==1) return head;
-        ListNode *start=head, *end=head;
-        int count=k-1;
-        while(count--){
-            end=end->next;
-            if(end==NULL)
-                return head;
+        ListNode *dummyNode = new ListNode(0);
+        dummyNode -> next = head;
+        ListNode *prev = dummyNode, *end = head;
+        int i = 0;
+        while(end){
+            i++;
+            //this means we need to reverse the linked list
+            if(i%k == 0){
+                ListNode *start = prev->next;
+                ListNode *nextPointer = end -> next;
+                reverse(start, end);
+                prev -> next = end;
+                start -> next = nextPointer;
+                prev = start;
+                end = nextPointer;
+            }
+            else{
+                end = end -> next;
+            }
         }
-        ListNode *newHead=reverseKGroup(end->next,k);
-        reverse(start,end);
-        start->next=newHead;
-        return end;
+        return dummyNode->next;
     }
 };
+
+/*Aproach
+
+1. Create one dummy node and point it to head and 1 prev node which will point to dummyNode
+2. Create a end node and iterate the linked list
+3. when the counter value is equal to k means we need to reverse the ll from prev->next(start node) to end node , before calling reversal end->next needs to be stored.
+4. After reversal pointer needs to be connected. Now connect prev->next to end and start->next to temp.
+5. Now for next iteration our prev eill be start and end will be temp.
+
+
+*/
