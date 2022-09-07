8b49eaa106c6856529f744ac26c8f4dbcb040aef
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2022-09-08 00:07:42 +0530

    Time: 81 ms (31.23%), Space: 17.8 MB (50.24%) - LeetHub

--- 143-reorder-list/143-reorder-list.cpp
+++ 143-reorder-list/143-reorder-list.cpp
@@ -10,7 +10,8 @@
  */
 class Solution {
 public:
-    void reorderList(ListNode* head) {
+    //Approach -1
+    /*reorder(ListNode * head){
         ListNode *curr = head, *prev=NULL, *nxt = NULL;
         while(curr && curr -> next){
             ListNode *saveNextNode = curr -> next;
@@ -24,6 +25,34 @@ public:
             if(saveNextNode != nxt)
                 nxt -> next = saveNextNode;
             curr = curr -> next -> next;
+    }*/
+    ListNode * reverse(ListNode * head){
+        ListNode *curr = head, *prev = NULL, *nxt;
+        while(curr){
+            nxt = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = nxt;
+        }
+        return prev;
+    }
+    void reorderList(ListNode* head) {
+        ListNode *slow = head, *fast = head -> next;
+        while(fast && fast -> next){
+            slow = slow -> next;
+            fast = fast -> next -> next;
+        }
+        ListNode *reversedHead = reverse(slow->next);
+        slow -> next = NULL;
+        slow = head;
+        
+        while(slow && reversedHead){
+            ListNode *snext = slow -> next;
+            ListNode * revNext = reversedHead -> next;
+            slow -> next = reversedHead;
+            reversedHead -> next = snext;
+            slow = snext;
+            reversedHead = revNext;
         }
     }
 };
