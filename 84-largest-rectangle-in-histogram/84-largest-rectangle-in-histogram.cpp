45fcd457187ab5a50a62cf0d7d5799c2efce658c
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2022-10-09 16:05:10 +0530

    Time: 464 ms (17.34%), Space: 87.5 MB (23.21%) - LeetHub

---
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    vector<int> nextSmaller(vector<int>& heights, int n){
+        stack<int> st;
+        st.push(-1);
+        vector<int> ans(n);
+        for(int i = n-1; i >= 0; i--){
+            while(st.top()!=-1 && heights[st.top()] >= heights[i])
+                st.pop();
+            ans[i] = st.top();
+            st.push(i);
+        }
+        return ans;
+    }
+    
+    vector<int> prevSmaller(vector<int> & heights, int n){
+        stack<int> st;
+        st.push(-1);
+        vector<int> ans(n);
+        for(int i = 0; i < n; i++){
+            while(st.top()!=-1 && heights[st.top()] >= heights[i])
+                st.pop();
+            ans[i] = st.top();
+            st.push(i);
+        }
+        return ans;
+    }
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        vector<int> leftSmaller(n);
+        leftSmaller = prevSmaller(heights, n);
+        
+        vector<int> rightSmaller(n);
+        rightSmaller = nextSmaller(heights, n);
+        
+        int maxArea = INT_MIN, newArea;
+        for(int i = 0 ; i < n ; i++){
+            if(rightSmaller[i] == -1)
+                rightSmaller[i] = n;
+            newArea = heights[i] * (rightSmaller[i] - leftSmaller[i] - 1);
+            maxArea = max(maxArea, newArea);
+        }
+        return maxArea;
+    }
+};
+
+/*Algo
+
+0) Create a prev smaller elements array
+1) Create a next smaller elements array 
+2) Max area rectangle for any index will be height_of_current_index * abs(left smaller index - right smaller index)
+
+*/
