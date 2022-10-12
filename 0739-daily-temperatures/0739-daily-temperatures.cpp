2f38a535d57380b742d78ebd408580dceae6f7af
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2022-10-12 20:04:42 +0530

    Time: 409 ms (15.53%), Space: 84.9 MB (96.36%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -6,33 +6,21 @@ public:
         stack<int> nextGreater;
         for(int i = temperatures.size() - 1; i >= 0; i--)
         {
-            if(!nextGreater.empty() && temperatures[nextGreater.top()] > temperatures[i])
+            while(!nextGreater.empty() && temperatures[i] >= temperatures[nextGreater.top()])
             {
-                numberofDays[i] = nextGreater.top() - i;
-                nextGreater.push(i);
+                nextGreater.pop();
             }
-            else if(!nextGreater.empty() && temperatures[i] >= temperatures[nextGreater.top()])
-            {
-                while(!nextGreater.empty() && temperatures[i] >= temperatures[nextGreater.top()])
-                {
-                    nextGreater.pop();
-                }
-                if(nextGreater.empty())
-                {
-                    numberofDays[i] = 0;
-                    nextGreater.push(i);
-                }
-                else
-                {
-                    numberofDays[i] = nextGreater.top() - i;
-                    nextGreater.push(i);
-                }  
-            }
-            else
+            if(nextGreater.empty())
             {
                 numberofDays[i] = 0;
                 nextGreater.push(i);
             }
+            else
+            {
+                numberofDays[i] = nextGreater.top() - i;
+                nextGreater.push(i);
+            }  
+            
         }
         return numberofDays;
     }
