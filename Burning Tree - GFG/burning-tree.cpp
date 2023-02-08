c3bd981356fbc30d2e47837fe10f3f408ddab849
sajalpandey <41550919+sajalpandey@users.noreply.github.com>
2023-02-08 20:29:08 +0530

    Added solution - LeetHub

---
+++ Burning Tree - GFG/burning-tree.cpp
@@ -0,0 +1,168 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+
+Node *buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current Node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current Node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+void buildParent(Node *root,int target, Node * &target_node,unordered_map<Node *, Node *> &parent){
+    queue<Node *> q;
+    q.push(root);
+    while(!q.empty()){
+        Node *curr = q.front();
+        q.pop();
+        if(curr->data==target)
+            target_node=curr;
+        if(curr->left)
+        {
+            parent[curr->left]=curr;
+            q.push(curr->left);
+        }
+        if(curr->right)
+        {
+            parent[curr->right]=curr;
+            q.push(curr->right);
+        }
+    }
+}
+class Solution {
+  public:
+    int minTime(Node* root, int target) 
+    {
+        unordered_map<Node *, Node *> parent;
+        Node *target_node;
+        buildParent(root,target,target_node,parent);
+        unordered_map<Node *,bool> visited;
+        queue<Node *> q;
+        q.push(target_node);
+        visited[target_node]=true;
+        int res = 0;
+        while(!q.empty()){
+            int size = q.size();
+            res++;
+            while(size--){
+                Node *curr = q.front();
+                q.pop();
+                if(curr->left && !visited[curr->left]){
+                    q.push(curr->left);
+                    visited[curr->left]=true;
+                }
+                if(curr->right && !visited[curr->right]){
+                    q.push(curr->right);
+                    visited[curr->right]=true;
+                }
+                if(parent[curr] && !visited[parent[curr]]){
+                    q.push(parent[curr]);
+                    visited[parent[curr]]=true;
+                }
+            }
+        }
+        return --res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+    int tc;
+    scanf("%d ", &tc);
+    while (tc--) 
+    {    
+        string treeString;
+        getline(cin, treeString);
+        // cout<<treeString<<"\n";
+        int target;
+        cin>>target;
+        // cout<<target<<"\n";
+
+        Node *root = buildTree(treeString);
+        Solution obj;
+        cout<<obj.minTime(root, target)<<"\n"; 
+
+        cin.ignore();
+
+    }
+
+
+    return 0;
+}
+
+// } Driver Code Ends
